381039bf49907aff7a55e6db5c4766e88a0c1ee2
nicek789 <nicek789@naver.com>
2021-04-26 19:12:46 +0900

    a

--- 신규아이디/solution.py
+++ 신규아이디/solution.py
@@ -1,11 +1,21 @@
+import re
+
+
 def is_valid(answer):
+    answer = answer.lower()
     for ch in answer:
-        if ch
+        if (ch.isalnum()):
+            continue
+        answer = re.sub(ch, "", answer)
+    return answer
 
 
 def solution(new_id):
     answer = ''
-    answer = new_id.lower()
+    answer = is_valid(new_id)
 
-    if
+    print(answer)
     return answer
+
+
+solution("123+ @-AAASD")

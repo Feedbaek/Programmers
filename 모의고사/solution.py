d86b85aa45bc72b49d5a0388f2f39aac08242bfa
nicek789 <nicek789@naver.com>
2021-04-28 17:17:36 +0900

    a

--- 모의고사/solution.py
+++ 모의고사/solution.py
@@ -3,16 +3,16 @@ def solution(answers):
     p1 = [1, 2, 3, 4, 5]
     p2 = [2, 1, 2, 3, 2, 4, 2, 5]
     p3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    person = {1: 0, 2: 0, 3: 0}
+    score = [0, 0, 0]
     for idx in range(len(answers)):
         if p1[idx % 5] == answers[idx]:
-            person[1] += 1
+            score[0] += 1
         if p2[idx % 8] == answers[idx]:
-            person[2] += 1
+            score[1] += 1
         if p3[idx % 10] == answers[idx]:
-            person[3] += 1
-    m = max(person.values())
-    for p in person.keys():
-        if person[p] == m:
-            answer.append(p)
+            score[2] += 1
+    max_score = max(score)
+    for p in range(len(score)):
+        if score[p] == max_score:
+            answer.append(p + 1)
     return answer

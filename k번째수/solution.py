4582d852e960c0fdd00d109b2043aeb0246bb02b
nicek789 <nicek789@naver.com>
2021-05-02 13:18:08 +0900

    a

--- k번째수/solution.py
+++ k번째수/solution.py
@@ -1,3 +1,8 @@
 def solution(array, commands):
     answer = []
+    tmp = []
+    for n in range(len(commands)):
+        tmp = array[commands[n][0] - 1:commands[n][1]]
+        tmp.sort()
+        answer.append(tmp[commands[n][2] - 1])
     return answer

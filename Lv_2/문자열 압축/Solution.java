fafef241f9e51a3d3310c87e364dab4f67f6f8c0
minskim2 <nicek789@gmail.com>
2021-07-21 21:47:56 +0900

    21.07.21

--- Lv_2/문자열 압축/Solution.java
+++ Lv_2/문자열 압축/Solution.java
@@ -2,34 +2,33 @@ import java.util.Arrays;
 
 class Solution {
 	public int solution(String s) {
-		int len = s.length();
-		int answer[];
-		answer =  new int[len];
-		for (int i = 1; i < len + 1; i++) {
-			String ret = "";
-			for (int j = 0; j < len;) {
-				int cnt = 1;
-				String str = "";
-				if (j + i < len) {
-					str = s.substring(j, j + i);
-					while (j+(i*(cnt + 1)-1) < len && str.equals(s.substring(j + (i * (cnt)), j + (i*(cnt+1))))){
-						cnt += 1;
-					}
-					if (cnt > 1) {
-						ret += String.valueOf(cnt);
-					}
-					ret += str;
+		int len = s.length();		// 문자열 총 길이
+		int answer[] =  new int[len];		// 압축 문자열 길이를 담을 배열
+		for (int i = 1; i < len + 1; i++) {		// 압축 문자열 길이
+			String ret = "";		// 압축 문자열 만들기
+			for (int j = 0; j < len;) {		// 크기 i 단위로 압축하기
+				int cnt = 1;		// 반복되는 문자열 개수
+				String str = "";		// 크기 i만큼 나눈 문자열 버퍼
+				if (j + i < len) {		// 크기 i만큼 나눌수 있으면
+					str = s.substring(j, j + i);		// i부터 j-1까지 자르기
+					while (j+(i*(cnt + 1)-1) < len &&
+					str.equals(s.substring(j + (i * (cnt)), j + (i*(cnt+1)))))
+							// 중복되는 문자열이 있으면 반복
+						cnt += 1;		// 중복 개수 카운트
+					if (cnt > 1)		// 중복이 존재하면
+						ret += String.valueOf(cnt);		// 문자열에  중복표시
+					ret += str;			// 압축 문자열에 버퍼크기의 문자 추가
 				}
-				else {
+				else {		// 나머지 문자열
 					str = s.substring(j);
-					ret += str;
+					ret += str;		// 압축 문자열에 추가
 				}
-				j += cnt * i;
+				j += cnt * i;		// 반복문 조건
 			}
-			answer[i - 1] = ret.length();
+			answer[i - 1] = ret.length();		// 리스트에 문자열 길이 대입
 		}
-		Arrays.sort(answer);
-		return answer[0];
+		Arrays.sort(answer);		// 오름차순 정렬
+		return answer[0];		// 가장 작은 값 반환
 	}
 	public static void main(String[] args) {
 		Solution s = new Solution();

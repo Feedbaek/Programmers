0a4bc79c7bd16a0db63d44cae35cc3eede174192
minskim2 <nicek789@gmail.com>
2021-07-22 17:51:40 +0900

    21.07.22

--- Lv_2/124 나라의 숫자/Solution.java
+++ Lv_2/124 나라의 숫자/Solution.java
@@ -1,9 +1,36 @@
 class Solution {
+	public char rec_3(int n, char[] s, StringBuilder sum) {
+		int x = n / 3;
+		if (n % 3 == 0 && x > 0)
+			x--;
+		if (x > 0)
+			sum.append(rec_3(x, s, sum));
+		return (s[(n - 1)%3]);
+	}
 	public String solution(int n) {
-		String answer = "";
-		return answer;
+		char s[] = {'1','2','4'};
+		StringBuilder sb = new StringBuilder();
+		sb.append(rec_3(n, s, sb));
+		return (sb.toString());
 	}
 	public static void main(String[] args){
-
+		Solution s = new Solution();
+		String r = s.solution(10);
+		System.out.println(r);
 	}
 }
+
+// 다른 사람 코드
+
+//class Solution {
+//	public String solution(int n) {
+//		String[] num = {"4","1","2"};
+//		String answer = "";
+
+//		while(n > 0){
+//			answer = num[n % 3] + answer;
+//			n = (n - 1) / 3;
+//		}
+//		return answer;
+//	}
+//}

c03f9c35d02ab22da0b846f0b7ba8ab993568831
minskim2 <nicek789@naver.com>
2021-07-29 22:43:22 +0900

    21.07.29

--- Lv_2/멀쩡한 사각형/Solution.java
+++ Lv_2/멀쩡한 사각형/Solution.java
@@ -1,9 +1,9 @@
 class Solution {
 	public long solution(long w, long h) {
 		long cnt = 0;
-		for (int i=0; i<w; i++)
-			cnt += ((h * (i + 1) + w - 1) / w) - (h * i / w);
-		return ((w * h) - cnt);
+		for (int i=0; i<w; i++)		// 가로 길이만큼 반복
+			cnt += ((h * (i + 1) + w - 1) / w) - (h * i / w);	// 지워지는 사각형
+		return ((w * h) - cnt);		// 전체 사각형 개수에서 뺸다
 	}
 	public static void main(String[] args) {
 		int w = 8;

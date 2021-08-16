14fa562ac14166cbac8a84466b3836d5740552d9
Feedbaek <nicek789@naver.com>
2021-08-16 19:53:24 +0900

    ing

---
+++ Weekly/퍼즐 조각 채우기/Solution.java
@@ -0,0 +1,10 @@
+class Solution {
+	public int solution(int[][] game_board, int[][] table) {
+		int answer = -1;
+		return answer;
+	}
+	public static void main(String[] args) {
+		Solution s = new Solution();
+		System.out.println(s.solution(game_board, table));
+	}
+}
